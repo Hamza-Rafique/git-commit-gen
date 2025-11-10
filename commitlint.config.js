bfeb633319dfc93a7acb037f738bc2a6367e4e56
Hamza Rafique <hamzarafique964@gmail.com>
2025-11-10 08:26:20 +0000

    feat: update commilint

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   extends: ['@commitlint/config-conventional'],
   rules: {
-    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore']],
+    'type-enum': [2, 'always', ['feat', 'fix', 'docs', 'chore', 'style', 'refactor', 'test', 'revert']],
     'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']]
   }
 };
